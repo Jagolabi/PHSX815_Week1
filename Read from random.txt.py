07bdfd7a73527ea9fd332d344151952bf678151f
Michael Chukwuka <109005549+Jagolabi@users.noreply.github.com>
2023-01-22 14:58:19 -0600

    Update Read from random.txt.py

--- Read from random.txt.py
+++ Read from random.txt.py
@@ -5,11 +5,11 @@ with open("random_numbers.txt", "r") as file:
     random_numbers = [float(line.strip()) for line in file]
 
 # Create a histogram of the numbers
-plt.hist(random_numbers, bins=50, density=True, facecolor='g', alpha=0.75)
+plt.hist(random_numbers, bins=50, density=True, facecolor='r', alpha=0.75)
 plt.xlabel('x')
-plt.ylabel('Probability')
+plt.ylabel('Expectancy')
 plt.title('Uniform random number')
-plt.grid(True)
+plt.grid(False)
 
 # Show the plot
 plt.show()
